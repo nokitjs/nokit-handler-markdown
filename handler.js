824d17e3941b202b21d9de634ce9c264d2367cab
Houfeng <admin@xhou.net>
2016-03-18 18:47:22 +0800

    重写

--- handler.js
+++ handler.js
@@ -1,49 +1,44 @@
-var markdown = require("./markdown");
 var fs = require("fs");
+var mditor = require("mditor");
+var tp = require("tpjs");
+
+var styleBuffer = fs.
+  readFileSync(require.resolve('mditor/build/css/mditor.min.css'))
+  .toString();
+var page = tp.compile(fs.readFileSync(__dirname + "/page.html").toString());
 
 var ALLOWED_METHODS = ["GET"];
 
 var Handler = module.exports = function(server) {
-    var self = this;
-    self.server = server;
-    self.configs = self.server.configs;
+  var self = this;
+  self.server = server;
+  self.configs = self.server.configs;
+  self.parser = new mditor.Parser();
 };
 
 //处理请求
 Handler.prototype.handle = function(context) {
-    var self = this;
-    context.request.physicalPathExists(function(exists) {
-        if (exists) {
-            // markdown 只允许 get 请求
-            if (ALLOWED_METHODS.indexOf(context.request.method) < 0) {
-                context.notAllowed();
-                return;
-            }
-            fs.readFile(context.request.physicalPath, function(err, data) {
-                if (err) {
-                    context.error(err);
-                } else {
-                    var style = self.readStyle();
-                    var body = markdown.toHTML(data.toString());
-                    var html = "<html>\r\n<head>\r\n<title>Markdown Preview</title>\r\n<meta charset=\"UTF-8\"/>\r\n<style>\r\n" + style + "\r\n</style>\r\n</head>\r\n<body>\r\n" + body + "\r\n</body>\r\n</html>";
-                    context.send(html, self.server.mime('.html'));
-                }
-            });
+  var self = this;
+  context.request.physicalPathExists(function(exists) {
+    if (exists) {
+      // markdown 只允许 get 请求
+      if (ALLOWED_METHODS.indexOf(context.request.method) < 0) {
+        context.notAllowed();
+        return;
+      }
+      fs.readFile(context.request.physicalPath, function(err, data) {
+        if (err) {
+          context.error(err);
         } else {
-            self.next(context);
+          var html = page({
+            style: styleBuffer,
+            body: self.parser.parse(data.toString())
+          });
+          context.send(html, self.server.mime('.html'));
         }
-    });
-};
-
-//读样式
-Handler.prototype.readStyle = function() {
-    var self = this;
-    if (self.styleCache) {
-        return self.styleCache;
+      });
+    } else {
+      self.next(context);
     }
-    var styleFile = __dirname + "/markdown.css";
-    if (fs.existsSync(styleFile)) {
-        self.styleCache = fs.readFileSync(styleFile);
-    }
-    return self.styleCache;
+  });
 };
